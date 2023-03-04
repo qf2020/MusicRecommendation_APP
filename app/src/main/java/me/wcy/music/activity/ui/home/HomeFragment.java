f3422c9be9e15be77db0db79dc4e37970a66f1e3
qf2020 <2698605548@qq.com>
2023-03-04 23:41:51 +0800

    1

--- app/src/main/java/me/wcy/music/activity/ui/home/HomeFragment.java
+++ app/src/main/java/me/wcy/music/activity/ui/home/HomeFragment.java
@@ -1,45 +1,97 @@
 package me.wcy.music.activity.ui.home;
 
+import android.animation.ArgbEvaluator;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.arch.lifecycle.ViewModelProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.wcy.music.R;
 import me.wcy.music.databinding.FragmentHomeBinding;
 
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
+    public static final int FRAGMENT_COUNT = 4;
+    private ImageView mBackImageView;
+    private TextView mTitleView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(HomeViewModel.class);
         //viewModel主要的作用是保持数据长久存在，可以在后续开发中思考使用
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        mBackImageView = binding.getRoot().findViewById(R.id.iv_back);
+        mTitleView = binding.getRoot().findViewById(R.id.tv_title);
        // final TextView textView = binding.textHome;
-        final ListView listView = binding.nestList;
-        String[] data={"菠萝","芒果","石榴","葡萄", "苹果", "橙子", "西瓜","菠萝","芒果","石榴","葡萄", "苹果", "橙子", "西瓜","菠萝","芒果","石榴","葡萄", "苹果", "橙子", "西瓜"};
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,data);
-        //5、将适配器加载到控件中
-        listView.setAdapter(adapter);
+        binding.slViewpager.setAdapter(new BaseFragmentItemAdapter(getActivity().getSupportFragmentManager(), initFragments(), initTitles()));
+        binding.slTab.setupWithViewPager(binding.slViewpager);
+        binding.sickLayout.setScrollChangeListener(new NestedScrollingParent2Layout.ScrollChangeListener() {
+            @Override
+            public void onScroll(float moveRatio) {
+                initToolBar(R.drawable.ic_start_mid, moveRatio);
+            }
+        });
 
         //使用自定义组件，下面部分显示不全，并且不显示bottomview
 
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
 
+    }
+    private void initToolBar(@DrawableRes int backResId, float moveRatio) {
+        mBackImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+        int color = (int) argbEvaluator.evaluate(moveRatio, Color.WHITE, Color.BLACK);
+        Drawable wrapDrawable = DrawableCompat.wrap(getResources().getDrawable(backResId));
+        DrawableCompat.setTint(wrapDrawable, color);
+
+        mBackImageView.setImageDrawable(wrapDrawable);
+        mTitleView.setAlpha(moveRatio);
+
+
+    }
+
+    private List<Fragment> initFragments() {
+        List<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i < FRAGMENT_COUNT; i++) {
+            fragments.add(TabFragment.newInstance("NestedScrolling2Demo"));
+        }
+        return fragments;
+    }
+    private List<String> initTitles() {
+        List<String> titles = new ArrayList<>();
+        titles.add("首页");
+        titles.add("全部");
+        titles.add("作者");
+        titles.add("专辑");
+        return titles;
     }
 
     @Override
--- app/src/main/java/me/wcy/music/activity/ui/home/NestedScrollingParent2Layout.java
+++ app/src/main/java/me/wcy/music/activity/ui/home/NestedScrollingParent2Layout.java
@@ -6,26 +6,27 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.NestedScrollingParent2;
 import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-public class NestedScrollingParent2Layout extends LinearLayout implements NestedScrollingParent2 {
+import me.wcy.music.R;
 
-    private View mTopView;
-    private View mContentView;
-    private View mBottomView;
-    private int mTopViewHeight;
-    private int mGap;
-    private int mBottomViewHeight;
+public class NestedScrollingParent2Layout extends LinearLayout implements NestedScrollingParent2 {
 
 
     private NestedScrollingParentHelper mNestedScrollingParentHelper = new NestedScrollingParentHelper(this);
-
-
-
+    private View mTopView;//头部view
+    private View mNavView;//导航view
+    private ViewPager mViewPager;//Viewpager
+    private ScrollChangeListener mScrollChangeListener;
+    /**
+     * 父控件可以滚动的距离
+     */
+    private float mCanScrollDistance = 0f;
 
     public NestedScrollingParent2Layout(Context context) {
         this(context, null);
@@ -39,137 +40,57 @@ public class NestedScrollingParent2Layout extends LinearLayout implements Nested
         super(context, attrs, defStyleAttr);
         setOrientation(LinearLayout.VERTICAL);
     }
+
     /**
-     * 即将开始嵌套滑动，此时嵌套滑动尚未开始，由子控件的 startNestedScroll 方法调用
-     *
-     * @param view  嵌套滑动对应的父类的子类(因为嵌套滑动对于的父控件不一定是一级就能找到的，可能挑了两级父控件的父控件，child的辈分>=target)    child
-     * @param view1 具体嵌套滑动的那个子类    target
-     * @param i   嵌套滑动支持的滚动方向  axes
-     * @param i1   嵌套滑动的类型，有两种ViewCompat.TYPE_NON_TOUCH fling效果,ViewCompat.TYPE_TOUCH 手势滑动   type
-     * @return true 表示此父类开始接受嵌套滑动，只有true时候，才会执行下面的 onNestedScrollAccepted 等操作
+     * 父控件接受嵌套滑动，不管是手势滑动还是fling 父控件都接受
      */
     @Override
-    public boolean onStartNestedScroll(@NonNull View view, @NonNull View view1, int i, int i1) {
-        if (mContentView != null && mContentView instanceof RecyclerView) {
-            ((RecyclerView) mContentView).stopScroll();
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
+        return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
+    @Override
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes, type);
+    }
+
+
+    @Override
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        //如果子view欲向上滑动，则先交给父view滑动
+        boolean hideTop = dy > 0 && getScrollY() < mCanScrollDistance;
+        //如果子view欲向下滑动，必须要子view不能向下滑动后，才能交给父view滑动
+        boolean showTop = dy < 0 && getScrollY() >= 0 && !target.canScrollVertically(-1);
+        if (hideTop || showTop) {
+            scrollBy(0, dy);
+            consumed[1] = dy;// consumed[0] 水平消耗的距离，consumed[1] 垂直消耗的距离
         }
-        mTopView.stopNestedScroll();
-        return (i & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
-
     }
 
-    /**
-     * 当onStartNestedScroll返回为true时，也就是父控件接受嵌套滑动时，该方法才会调用
-     *
-     * @param view
-     * @param view1
-     * @param i
-     * @param i1
-     */
-    @Override
-    public void onNestedScrollAccepted(@NonNull View view, @NonNull View view1, int i, int i1) {
-        mNestedScrollingParentHelper.onNestedScrollAccepted(view, view1, i, i1);
-    }
-
-    /**
-     * 停止滑动
-     *
-     * @param target
-     * @param type
-     */
-    @Override
-    public void onStopNestedScroll(@NonNull View target, int type) {
-        if (type == ViewCompat.TYPE_NON_TOUCH) {
-            System.out.println("onStopNestedScroll");
-        }
-
-        mNestedScrollingParentHelper.onStopNestedScroll(target, type);
-
-    }
-
-    /**
-     * 在 onNestedPreScroll 中，父控件消耗一部分距离之后，剩余的再次给子控件，
-     * 子控件消耗之后，如果还有剩余，则把剩余的再次还给父控件
-     *
-     * @param target       具体嵌套滑动的那个子类
-     * @param dxConsumed   水平方向嵌套滑动的子控件滑动的距离(消耗的距离)
-     * @param dyConsumed   垂直方向嵌套滑动的子控件滑动的距离(消耗的距离)
-     * @param dxUnconsumed 水平方向嵌套滑动的子控件未滑动的距离(未消耗的距离)
-     * @param dyUnconsumed 垂直方向嵌套滑动的子控件未滑动的距离(未消耗的距离)
-     */
 
     @Override
     public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
-        if (dyUnconsumed<0){
-            //对于向下滑动
-            if (target == mBottomView){
-                mContentView.scrollBy(0, dyUnconsumed);
-            }
-        }else {
-            if (target == mTopView){
-                mContentView.scrollBy(0, dyUnconsumed);
-            }
+        if (dyUnconsumed < 0) {//表示已经向下滑动到头，这里不用区分手势还是fling
+            scrollBy(0, dyUnconsumed);
         }
+    }
+
+    @Override
+    public void onStopNestedScroll(@NonNull View target, int type) {
+        mNestedScrollingParentHelper.onStopNestedScroll(target, type);
     }
 
     /**
-     * 在子控件开始滑动之前，会先调用父控件的此方法，由父控件先消耗一部分滑动距离，并且将消耗的距离存在consumed中，传递给子控件
-     * 在嵌套滑动的子View未滑动之前
-     * ，判断父view是否优先与子view处理(也就是父view可以先消耗，然后给子view消耗）
-     *
-     * @param target   具体嵌套滑动的那个子类
-     * @param dx       水平方向嵌套滑动的子View想要变化的距离
-     * @param dy       垂直方向嵌套滑动的子View想要变化的距离 dy<0向下滑动 dy>0 向上滑动
-     * @param consumed 这个参数要我们在实现这个函数的时候指定，回头告诉子View当前父View消耗的距离
-     *                 consumed[0] 水平消耗的距离，consumed[1] 垂直消耗的距离 好让子view做出相应的调整
-     * @param type     滑动类型，ViewCompat.TYPE_NON_TOUCH fling效果,ViewCompat.TYPE_TOUCH 手势滑动
+     * 嵌套滑动时，如果父View处理了fling,那子view就没有办法处理fling了，所以这里要返回为false
      */
     @Override
-    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
-
-        //这里不管手势滚动还是fling都处理
-        boolean hideTop = dy > 0 && getScrollY() < mTopViewHeight ;
-        boolean showTop = dy < 0
-                && getScrollY() >= 0
-                && !target.canScrollVertically(-1)
-                && !mContentView.canScrollVertically(-1)
-                &&target!=mBottomView
-                ;
-        boolean cunsumedTop = hideTop || showTop;
-
-        //对于底部布局
-        boolean hideBottom = dy < 0 && getScrollY() > mTopViewHeight;
-        boolean showBottom = dy > 0
-                && getScrollY() >= mTopViewHeight
-                && !target.canScrollVertically(1)
-                && !mContentView.canScrollVertically(1)
-                &&target!=mTopView
-                ;
-        boolean cunsumedBottom = hideBottom || showBottom;
-
-        if (cunsumedTop) {
-            scrollBy(0, dy);
-            consumed[1] = dy;
-        } else if (cunsumedBottom) {
-            scrollBy(0, dy);
-            consumed[1] = dy;
-        }
-
+    public boolean onNestedPreFling(@NonNull View target, float velocityX, float velocityY) {
+        return false;
     }
 
     @Override
-    public int getNestedScrollAxes() {
-        return mNestedScrollingParentHelper.getNestedScrollAxes();
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //ViewPager修改后的高度= 总高度-导航栏高度
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        ViewGroup.LayoutParams layoutParams = mContentView.getLayoutParams();
-        layoutParams.height = getMeasuredHeight();
-        mContentView.setLayoutParams(layoutParams);
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    public boolean onNestedFling(@NonNull View target, float velocityX, float velocityY, boolean consumed) {
+        return false;
     }
 
     @Override
@@ -179,46 +100,63 @@ public class NestedScrollingParent2Layout extends LinearLayout implements Nested
             mTopView = getChildAt(0);
         }
         if (getChildCount() > 1) {
-            mContentView = getChildAt(1);
+            mNavView = getChildAt(1);
         }
         if (getChildCount() > 2) {
-            mBottomView = getChildAt(2);
+            mViewPager = (ViewPager) getChildAt(2);
         }
 
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //先测量一次
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //ViewPager修改后的高度= 总高度-TabLayout高度
+        ViewGroup.LayoutParams lp = mViewPager.getLayoutParams();
+        lp.height = getMeasuredHeight() - mNavView.getMeasuredHeight();
+        mViewPager.setLayoutParams(lp);
+        //因为ViewPager修改了高度，所以需要重新测量
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        if (mTopView != null) {
-            mTopViewHeight = mTopView.getMeasuredHeight() ;
-        }
-        if (mBottomView != null) {
-            mBottomViewHeight = mBottomView.getMeasuredHeight();
-        }
+        mCanScrollDistance = mTopView.getMeasuredHeight() - getResources().getDimension(R.dimen.normal_title_height);
     }
 
     @Override
     public void scrollTo(int x, int y) {
-
         if (y < 0) {
             y = 0;
         }
+        if (y > mCanScrollDistance) {
+            y = (int) mCanScrollDistance;
+        }
+        if (mScrollChangeListener != null) {
+            mScrollChangeListener.onScroll(y / mCanScrollDistance);
+        }
+        if (getScrollY() != y) super.scrollTo(x, y);
+    }
 
-        //对滑动距离进行修正
-        if (mContentView.canScrollVertically(1)) {
-            //可以向上滑栋
-            if (y > mTopViewHeight) {
-                y = mTopViewHeight-mGap;
-            }
-        } else if ((mContentView.canScrollVertically(-1))) {
-            if (y < mTopViewHeight) {
-                y = mTopViewHeight+mGap ;
-            }
-        }
-        if (y > mTopViewHeight + mBottomViewHeight) {
-            y = mTopViewHeight + mBottomViewHeight;
-        }
-        super.scrollTo(x, y);
+
+    @Override
+    public int getNestedScrollAxes() {
+        return mNestedScrollingParentHelper.getNestedScrollAxes();
+    }
+
+
+    public interface ScrollChangeListener {
+        /**
+         * 移动监听
+         *
+         * @param moveRatio 移动比例
+         */
+        void onScroll(float moveRatio);
+    }
+
+    public void setScrollChangeListener(ScrollChangeListener scrollChangeListener) {
+        mScrollChangeListener = scrollChangeListener;
     }
 }
